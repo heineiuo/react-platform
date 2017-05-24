5c7caf786e50facbcd93feba7aa304a53ccf41de
heineiuo <heineiuo@gmail.com>
2017-05-24 11:44:44 +0800

    Release v0.0.1

--- README.md
+++ README.md
@@ -1,1 +1,75 @@
 # react-platform-js
+
+## Install
+
+```bash
+yarn add react-platform-js # or npm install react-platform-js
+```
+
+## Usage
+
+### method
+```javascript
+import Platform from 'react-platform-js
+
+Platform.OS // OS name, Mac OS
+Platform.OSVersion // OS version, 10.11
+Platform.Browser // Browser name, Chrome
+Platform.BrowserVersion // Browser Version
+Platform.Engine // browser engine name
+// and ...
+Platform.CPU
+Platform.DeviceType
+Platform.DeviceModel
+Platform.DeviceVendor
+Platform.UA
+
+```
+
+### component
+
+```javascript
+import React, {Component} from 'react'
+import Platform from 'react-platform-js'
+
+class App extends Component {
+  render() {
+    return (
+      <Platform rules={{OS: 'Mac OS'}}>
+        Only Mac OS computer will see this text.
+      </Platform>
+    )
+  }
+}
+
+class App2 extends Component {
+  render() {
+    return (
+      <Platform rules={{OS: 'Mac OS'}}>
+        {props => {
+          return (
+            <div>
+              OS: {props.OS},
+              OSVersion: {props.OSVersion},
+              Browser: {props.Browser},
+              BrowserVersion: {props.BrowserVersion},
+              DeviceType: {props.DeviceType},
+              DeviceModel: {props.DeviceModel},
+              DeviceVendor: {props.DeviceVendor},
+              Engine: {props.Engine},
+              EngineVersion: {props.EngineVersion},
+              CPU: {props.CPU},
+              UA: {props.UA},
+            </div>
+          )
+        }}
+      </Platform>
+    )
+  }
+}
+
+```
+
+## License
+
+MIT
---
+++ src/index.js
@@ -0,0 +1,65 @@
+import React, {Component} from 'react'
+import UAParser from 'ua-parser-js'
+
+const parser = new UAParser()
+const result = parser.getResult()
+
+const OS = result.os.name
+const OSVersion = result.os.version
+const Browser = result.browser.name
+const BrowserVersion = result.browser.version
+const DeviceType = result.device.type
+const DeviceModel= result.device.model
+const DeviceVendor = result.device.vendor
+const Engine = result.engine.name
+const EngineVersion = result.engine.version
+const CPU = result.cpu.architecture
+const UA = result.ua
+
+const props = {
+  OS,
+  OSVersion,
+  Browser,
+  BrowserVersion,
+  DeviceType,
+  DeviceModel,
+  DeviceVendor,
+  Engine,
+  EngineVersion,
+  CPU,
+  UA
+}
+
+class Platform extends Component {
+  static OS = OS
+  static OSVersion = OSVersion
+  static Browser = Browser
+  static BrowserVersion = BrowserVersion
+  static DeviceType = DeviceType
+  static DeviceModel= DeviceModel
+  static DeviceVendor = DeviceVendor
+  static Engine = Engine
+  static EngineVersion = EngineVersion
+  static CPU = CPU
+  static UA = UA
+
+  static defaultProps = {
+    rules: {}
+  }
+
+  static select = (selectMap) => {
+    return Object.keys(selectMap).find(key => {
+      return key === this.OS
+    })
+  }
+
+  render () {
+    const {children, rules} = this.props
+    if (typeof children === 'function') return children(props)
+    const illegal = Object.keys(rules).find(key => props[key] !== rules[key])
+    if (illegal) return null;
+    return React.cloneElement(children)
+  }
+}
+
+export default Platform
